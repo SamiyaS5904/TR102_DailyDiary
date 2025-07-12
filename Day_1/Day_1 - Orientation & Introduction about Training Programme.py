f54b2f49935ae1f8fc2f25ec5e0828c79734ec15
Samiya Sehgal <samiyasehgal@gmail.com>
2025-07-12 23:13:47 +0530

    Organised Daywise Folders for week 1

---
+++ Day_1/Day_1 - Orientation & Introduction about Training Programme.py
@@ -0,0 +1,11 @@
+# Day 1 - Industrial Training 102 at Auribises Technologies
+
+# I explored Python basics today
+# Installed Python and set it up on my device
+# Executed my first simple Python program
+
+# This is a simple Python program to print a message
+
+print("Hii, it's my Day 1 of Industrial Training 102 at Auribises Technologies 🚀")
+print("I installed Python, set it up, and successfully ran my first program today! 🎉")
+
