8555620961ce1434aad874c703d635e9420ec104
Samiya Sehgal <samiyasehgal@gmail.com>
2025-07-13 17:17:45 +0530

    Training Daily Dairy Codes updated

--- script.js
+++ script.js
@@ -504,13 +504,3 @@ function initScrollToTop() {
 
 // Initialize scroll to top
 document.addEventListener('DOMContentLoaded', initScrollToTop);
-
-
-function toggleLogs(id) {
-    const el = document.getElementById(id);
-    if (el.classList.contains('hidden')) {
-        el.classList.remove('hidden');
-    } else {
-        el.classList.add('hidden');
-    }
-}
