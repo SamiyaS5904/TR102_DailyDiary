c40306f5f60cb3678d64d4920667befa11b5d8a1
Samiya Sehgal <samiyasehgal@gmail.com>
2025-07-14 23:19:32 +0530

    File Handling

--- Day_19/Day_19.5.py
+++ Day_19/Day_19.5.py
@@ -1,3 +1,8 @@
+# PERSISITENCE : TO SAVE DATA FROM PRIMARY SOURCE TO SECONDARY (FILE/DATABASE)
+    #FILE - WHEN LESS DATA 
+    # DATABASE - TREE DATA STRUCTURE
+  #               PROFESSIONALY SYSTEM, HIGH USE, UPDATES REQUIRED, SECURITY REQUIRED
+
 # name = input('enter your name: ')
 # person_to_meet = input('enter person name you want to meet: ')
 # purpose = input('enter your purpose to meet: ')
@@ -7,7 +12,7 @@
 # This file read/write operation will work on plain text files
 # i.e. text files (eg: .txt, .md, .py, .c, .cpp, .json etc etc)
 # it wont work for docx, pdf, pptx
-file = open('Session1.py', 'r')
+file = open('Day_19.py', 'r')
 # data = file.read()
 lines = file.readlines()
 print(len(lines))
@@ -16,8 +21,11 @@ print(len(lines))
 #     print(line)
 
 # Read the files, from any path of your OS Location
-file = open('/Users/ishant/Downloads/Hero-Eco-Tech.txt', 'r')
+# file = open('/Users/ishant/Downloads/Hero-Eco-Tech.txt', 'r')
 print(file.read())
+
+file = open('D:\C C++ JCC\18.7.23 LOOPING\ap upto n terms.c', 'r')
+
 
 # HW: explore how to use with keyword for file handling
 #     why to use with, if we can directly access open function
---
+++ Day_19/Day_19_6.py
@@ -0,0 +1,5 @@
+file = open('Day_19/Day_19.py', 'r')                    # use proper file location 
+lines = file.readlines()
+
+for kuchbhi in lines:
+    print(kuchbhi)
