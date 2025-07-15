6ecb96d98857bb1de3088733ea1ebe132dc7c229
Samiya Sehgal <samiyasehgal@gmail.com>
2025-07-15 11:30:49 +0530

    Doctor's App in Progress

---
+++ Day_22/Day_22_2.py
@@ -0,0 +1,10 @@
+from Day_22_1 import DoctorsApp
+
+# main file or driver file of the application
+def main():
+    app = DoctorsApp()
+    app.show_main_menu()
+
+
+if __name__ == '__main__':
+    main()
